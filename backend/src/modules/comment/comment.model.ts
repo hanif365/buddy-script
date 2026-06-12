9189eca8b2a2c89fc57aeaf1c99ba9c7a152d483
Abu Hanif <abuhanif.cse3@gmail.com>
2026-06-12 18:47:45 +0600

    features/comment model with replies

---
+++ backend/src/modules/comment/comment.model.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema(
+  {
+    post: { type: mongoose.Schema.Types.ObjectId, ref: "Post", required: true },
+    author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    parent: { type: mongoose.Schema.Types.ObjectId, ref: "Comment", default: null },
+    text: { type: String, required: true, trim: true },
+    likeCount: { type: Number, default: 0 },
+    replyCount: { type: Number, default: 0 },
+  },
+  { timestamps: true }
+);
+
+commentSchema.index({ post: 1, parent: 1, createdAt: -1 });
+
+const Comment = mongoose.model("Comment", commentSchema);
+
+export default Comment;
